13bfa6f041d12ce8eb1d6d9cca0949baaf06bf07
Daniel Cousens <dcousens@users.noreply.github.com>
2015-05-19 14:29:32 +1000

    no space

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var base58 = require('../')(BASE58)
 var fixtures = require('./fixtures.json')
 
 describe('bs*', function () {
-  describe('encode base 58', function () {
+  describe('encode base58', function () {
     fixtures.valid.forEach(function (f) {
       it('can encode ' + f.hex, function () {
         var actual = base58.encode(new Buffer(f.hex, 'hex'))
