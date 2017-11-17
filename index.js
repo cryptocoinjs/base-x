472571ed0521c5fdc7226f5cb78f0dbfffbde743
Ryan Bell <ryan@piing.net>
2017-11-17 02:02:43 -0800

    Update index.js
    Changed var -> let, added case for null & undefined.

--- index.js
+++ index.js
@@ -6,27 +6,28 @@
 // Merged Buffer refactorings from base58-native by Stephen Pair
 // Copyright (c) 2013 BitPay Inc
 
-var Buffer = require('safe-buffer').Buffer
+let Buffer = require('safe-buffer').Buffer
 
 module.exports = function base (ALPHABET) {
-  var ALPHABET_MAP = {}
-  var BASE = ALPHABET.length
-  var LEADER = ALPHABET.charAt(0)
+  let ALPHABET_MAP = {}
+  let BASE = ALPHABET.length
+  let LEADER = ALPHABET.charAt(0)
 
   // pre-compute lookup table
-  for (var z = 0; z < ALPHABET.length; z++) {
-    var x = ALPHABET.charAt(z)
+  for (let z = 0; z < ALPHABET.length; z++) {
+    let x = ALPHABET.charAt(z)
 
     if (ALPHABET_MAP[x] !== undefined) throw new TypeError(x + ' is ambiguous')
     ALPHABET_MAP[x] = z
   }
 
-  function encode (source) {
-    if (source.length === 0) return ''
+  function encode (src) {
+    if (!src || src.length === 0) return ''
 
-    var digits = [0]
-    for (var i = 0; i < source.length; ++i) {
-      for (var j = 0, carry = source[i]; j < digits.length; ++j) {
+    let digits = [0]
+    for (let i = 0; i < src.length; ++i) {
+			let carry = src[i];
+      for (let j = 0; j < digits.length; ++j) {
         carry += digits[j] << 8
         digits[j] = carry % BASE
         carry = (carry / BASE) | 0
@@ -38,25 +39,25 @@ module.exports = function base (ALPHABET) {
       }
     }
 
-    var string = ''
+    let str = ''
 
     // deal with leading zeros
-    for (var k = 0; source[k] === 0 && k < source.length - 1; ++k) string += ALPHABET[0]
+    for (let k = 0; src[k] === 0 && k < src.length - 1; ++k) str += ALPHABET[0]
     // convert digits to a string
-    for (var q = digits.length - 1; q >= 0; --q) string += ALPHABET[digits[q]]
+    for (let q = digits.length - 1; q >= 0; --q) str += ALPHABET[digits[q]]
 
-    return string
+    return str
   }
 
-  function decodeUnsafe (string) {
-    if (string.length === 0) return Buffer.allocUnsafe(0)
+  function decodeUnsafe (str) {
+    if (!str || str.length === 0) return Buffer.allocUnsafe(0)
 
-    var bytes = [0]
-    for (var i = 0; i < string.length; i++) {
-      var value = ALPHABET_MAP[string[i]]
+    let bytes = [0]
+    for (let i = 0; i < str.length; i++) {
+      let value = ALPHABET_MAP[str[i]]
       if (value === undefined) return
-
-      for (var j = 0, carry = value; j < bytes.length; ++j) {
+			let carry = value;
+      for (let j = 0; j < bytes.length; ++j) {
         carry += bytes[j] * BASE
         bytes[j] = carry & 0xff
         carry >>= 8
@@ -69,15 +70,13 @@ module.exports = function base (ALPHABET) {
     }
 
     // deal with leading zeros
-    for (var k = 0; string[k] === LEADER && k < string.length - 1; ++k) {
-      bytes.push(0)
-    }
+    for (let k = 0; str[k] === LEADER && k < str.length - 1; ++k) bytes.push(0)
 
     return Buffer.from(bytes.reverse())
   }
 
-  function decode (string) {
-    var buffer = decodeUnsafe(string)
+  function decode (str) {
+    let buffer = decodeUnsafe(str)
     if (buffer) return buffer
 
     throw new Error('Non-base' + BASE + ' character')
