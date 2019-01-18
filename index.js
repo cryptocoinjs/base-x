4259be7e366c9ce047e9038d1870b479c10fc032
John Jayasingh S <johnjayasingh.s@gmail.com>
2019-01-18 14:59:55 +0530

    Update index.js
    In few android platforms running this causes in error

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const Buffer = require('safe-buffer').Buffer
 module.exports = function base (ALPHABET) {
   if (ALPHABET.length >= 255) throw new TypeError('Alphabet too long')
 
-  const BASE_MAP = new Uint8Array(256)
-  BASE_MAP.fill(255)
+  const BASE_MAP = new Uint8Array(256); for (let i = 0; i < 256; i++) { BASE_MAP[i] = 255; }
 
   for (let i = 0; i < ALPHABET.length; i++) {
     const x = ALPHABET.charAt(i)
