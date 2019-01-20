c1158ae3d346e70eb99c1419e3cab662a617ae04
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2019-01-20 19:34:41 +0000

    adhere to standard style

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const Buffer = require('safe-buffer').Buffer
 module.exports = function base (ALPHABET) {
   if (ALPHABET.length >= 255) throw new TypeError('Alphabet too long')
 
-  const BASE_MAP = new Uint8Array(256);
+  const BASE_MAP = new Uint8Array(256)
   for (let i = 0; i < 256; i++) {
-    BASE_MAP[i] = 256;
+    BASE_MAP[i] = 256
   }
 
   for (let i = 0; i < ALPHABET.length; i++) {
