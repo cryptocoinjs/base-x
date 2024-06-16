2afb650a58bf2700a9aad6769486026c82175674
ayman <aymanshaik1015@gmail.com>
2024-06-16 18:14:45 +0530

    fix: rm .d.ts from esm folder

--- src/esm/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare function base(ALPHABET: string): base.BaseConverter;
-export default base;
-declare namespace base {
-    interface BaseConverter {
-        encode(buffer: Uint8Array | number[]): string;
-        decodeUnsafe(string: string): Uint8Array | undefined;
-        decode(string: string): Uint8Array;
-    }
-}
