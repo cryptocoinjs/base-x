e848733e5ef913368f0c52d0fb73673ae8e1161f
Steven Luscher <steven.luscher@anza.xyz>
2025-03-07 23:39:00 +0000

    Prohibit char codes that would overflow the `BASE_MAP`

--- src/index.js
+++ src/index.js
@@ -81,8 +81,12 @@ function base (ALPHABET) {
     var b256 = new Uint8Array(size)
         // Process the characters.
     while (source[psz]) {
+            // Find code of next character
+      var charCode = source.charCodeAt(psz)
+            // Base map can not be indexed using char code
+      if (charCode > 255) { return }
             // Decode character
-      var carry = BASE_MAP[source.charCodeAt(psz)]
+      var carry = BASE_MAP[charCode]
             // Invalid character
       if (carry === 255) { return }
       var i = 0
